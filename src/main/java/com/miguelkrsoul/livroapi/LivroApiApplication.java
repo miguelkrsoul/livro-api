fa9ade6f089d0565af7db03368e3a33af616b535
Miguel. Krsoul <miguelkrsoul@gmail.com>
2026-09-08 13:02:08 -0400

    Create LivroApiApplication class for Spring Boot app

---
+++ src/main/java/com/miguelkrsoul/livroapi/LivroApiApplication.java
@@ -0,0 +1,11 @@
+package com.miguelkrsoul.livroapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivroApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LivroApiApplication.class, args);
+    }
+}
