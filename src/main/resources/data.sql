04617e31a91d60aa8e8bae3b4905646267019675
Miguel. Krsoul <miguelkrsoul@gmail.com>
2026-09-10 09:37:12 -0400

    Update book entries in data.sql with new URLs and descriptions

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,24 +1,6 @@
 DELETE FROM livro;
 
 INSERT INTO livro (titulo, autor, capa_url, descricao, conteudo) VALUES 
-('Dom Casmurro', 'Machado de Assis', 'https://upload.wikimedia.org/wikipedia/commons/6/6e/Dom_Casmurro.jpg', 'Obra-prima sobre ciúme e Capitu. Será que Capitu traiu ou não traiu?', 'CAPÍTULO I - DO TÍTULO
-
-Uma noite destas, vindo da cidade para o Engenho Novo, encontrei no trem da Central um rapaz aqui do bairro, que eu conheço de vista e de chapéu.
-
-Cumprimentou-me, sentou-se ao pé de mim, falou da Lua e dos Ministros, e acabou recitando-me versos.
-
-CAPÍTULO II - DO LIVRO
-
-Agora que expliquei o título, passo a escrever o livro...'),
-
-('Vidas Secas', 'Graciliano Ramos', 'https://upload.wikimedia.org/wikipedia/pt/5/55/Vidas_Secas_%281938%29.jpg', 'A luta de Fabiano e sua família no sertão nordestino.', 'Na planície avermelhada, os juazeiros alargavam duas manchas verdes.
-
-Fabiano e sua família caminhavam pelo sertão, fugindo da seca.
-
-O menino mais velho perguntou: - Onde fica a nossa casa?
-
-Fabiano não sabia...'),
-
-('O Cortiço', 'Aluísio Azevedo', 'https://upload.wikimedia.org/wikipedia/pt/e/eb/O_Corti%C3%A7o_%281890%29.jpg', 'Um cortiço no Rio de Janeiro e suas histórias.', 'João Romão era um homem muito trabalhador e ambicioso.
-
-Dono de uma venda e de um cortiço, ele explorava seus moradores para ficar cada vez mais rico.');
+('Dom Casmurro', 'Machado de Assis', 'https://covers.openlibrary.org/b/id/10521270-L.jpg', 'Será que Capitu traiu?', 'CAPÍTULO I...'),
+('Vidas Secas', 'Graciliano Ramos', 'https://covers.openlibrary.org/b/id/8269446-L.jpg', 'Luta no sertão', 'Na planície avermelhada...'),
+('O Cortiço', 'Aluísio Azevedo', 'https://covers.openlibrary.org/b/id/12749851-L.jpg', 'Histórias de um cortiço', 'João Romão era...');
