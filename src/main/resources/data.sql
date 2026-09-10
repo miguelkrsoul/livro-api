b215ca27dcfd5c4fe2c1060b1adda86f6410a1fd
Miguel. Krsoul <miguelkrsoul@gmail.com>
2026-09-10 08:30:14 -0400

    Add initial book entries to data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO livro (titulo, autor, capa_url, descricao, conteudo) VALUES 
+('Dom Casmurro', 'Machado de Assis', 'https://covers.openlibrary.org/b/id/8264456-L.jpg', 'Clássico da literatura brasileira sobre ciúme e Capitu.', 'Capítulo 1 - Do título... Era uma vez... (aqui vai o texto do livro para passar o dedo e ler)'),
+('O Cortiço', 'Aluísio Azevedo', 'https://covers.openlibrary.org/b/id/8231856-L.jpg', 'A vida em um cortiço no Rio de Janeiro do século 19.', 'João Romão era dono do cortiço... (texto do livro)'),
+('Vidas Secas', 'Graciliano Ramos', 'https://covers.openlibrary.org/b/id/8306096-L.jpg', 'A luta de uma família de retirantes no sertão.', 'Na planície avermelhada... (texto do livro)');
