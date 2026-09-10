d223e77ff700080f7778df6cf2cc0643a2623b75
Miguel. Krsoul <miguelkrsoul@gmail.com>
2026-09-10 12:24:11 -0400

    Add update method to LivroController

--- src/main/java/com/miguelkrsoul/livroapi/controller/LivroController.java
+++ src/main/java/com/miguelkrsoul/livroapi/controller/LivroController.java
@@ -27,6 +27,16 @@ public class LivroController {
     @DeleteMapping("/{id}")
     public void deletar(@PathVariable Long id) {
         repo.deleteById(id);
+    }     @PutMapping("/{id}")
+    public Livro atualizar(@PathVariable Long id, @RequestBody Livro livroAtualizado) {
+        return repo.findById(id).map(l -> {
+            l.setTitulo(livroAtualizado.getTitulo());
+            l.setAutor(livroAtualizado.getAutor());
+            l.setCapaUrl(livroAtualizado.getCapaUrl());
+            l.setDescricao(livroAtualizado.getDescricao());
+            l.setConteudo(livroAtualizado.getConteudo());
+            return repo.save(l);
+        }).orElseThrow();
     }
 
     @GetMapping("/{id}")
