f1be03baa4c93d70cbea7a3a3188a41d02059382
Miguel. Krsoul <miguelkrsoul@gmail.com>
2026-09-10 09:42:52 -0400

    Modify book data in data.sql
    Updated book entries with new cover URLs and descriptions.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 DELETE FROM livro;
 
 INSERT INTO livro (titulo, autor, capa_url, descricao, conteudo) VALUES 
-('Dom Casmurro', 'Machado de Assis', 'https://covers.openlibrary.org/b/id/10521270-L.jpg', 'Será que Capitu traiu?', 'CAPÍTULO I...'),
-('Vidas Secas', 'Graciliano Ramos', 'https://covers.openlibrary.org/b/id/8269446-L.jpg', 'Luta no sertão', 'Na planície avermelhada...'),
-('O Cortiço', 'Aluísio Azevedo', 'https://covers.openlibrary.org/b/id/12749851-L.jpg', 'Histórias de um cortiço', 'João Romão era...');
+('Dom Casmurro', 'Machado de Assis', 'https://covers.openlibrary.org/b/id/10558903-L.jpg', 'Obra-prima sobre ciúme e Capitu', 'CAPÍTULO I - DO TÍTULO...'),
+('Vidas Secas', 'Graciliano Ramos', 'https://upload.wikimedia.org/wikipedia/pt/thumb/5/55/Vidas_Secas_%281938%29.jpg/200px-Vidas_Secas_%281938%29.jpg', 'Luta no sertão nordestino', 'Na planície avermelhada...'),
+('O Cortiço', 'Aluísio Azevedo', 'https://upload.wikimedia.org/wikipedia/pt/thumb/e/eb/O_Corti%C3%A7o_%281890%29.jpg/200px-O_Corti%C3%A7o_%281890%29.jpg', 'Histórias de um cortiço', 'João Romão era um homem trabalhador...');
