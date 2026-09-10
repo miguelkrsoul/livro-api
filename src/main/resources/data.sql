82b5c6be10554d439a8b39c2ae89c96b6dd9ae80
Miguel. Krsoul <miguelkrsoul@gmail.com>
2026-09-10 09:05:23 -0400

    Update book entries in data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,16 +1,8 @@
 DELETE FROM livro;
 
 INSERT INTO livro (titulo, autor, capa_url, descricao, conteudo) VALUES 
-('Dom Casmurro', 'Machado de Assis', 'https://m.media-amazon.com/images/I/71OZY035QKL.jpg', 'Obra-prima sobre ciúme e Capitu. Será que Capitu traiu ou não traiu?', 'CAPÍTULO I - DO TÍTULO
+('Dom Casmurro', 'Machado de Assis', 'https://covers.openlibrary.org/b/id/12662293-L.jpg', 'Obra-prima sobre ciúme e Capitu. Será que Capitu traiu ou não traiu?', 'CAPÍTULO I - DO TÍTULO\n\nUma noite destas, vindo da cidade para o Engenho Novo, encontrei no trem da Central um rapaz aqui do bairro...'),
 
-Uma noite destas, vindo da cidade para o Engenho Novo, encontrei no trem da Central um rapaz aqui do bairro, que eu conheço de vista e de chapéu. Cumprimentou-me, sentou-se ao pé de mim, falou da Lua e dos Ministros, e acabou recitando-me versos.
+('Vidas Secas', 'Graciliano Ramos', 'https://covers.openlibrary.org/b/id/12749873-L.jpg', 'A luta de Fabiano e sua família no sertão nordestino.', 'Na planície avermelhada, os juazeiros alargavam duas manchas verdes...'),
 
-Eram versos de um poeta... eu não me lembrava mais...
-
-Continue lendo rolando pra baixo... Esse é o início real do livro Dom Casmurro. Você pode colar o livro inteiro aqui que a pessoa vai conseguir passar o dedo e ler!'),
-
-('Vidas Secas', 'Graciliano Ramos', 'https://m.media-amazon.com/images/I/81Q+4h2mQKL.jpg', 'A luta de Fabiano e sua família no sertão nordestino.', 'Na planície avermelhada, os juazeiros alargavam duas manchas verdes...
-
-Fabiano e sua família caminhavam pelo sertão...'),
-
-('O Cortiço', 'Aluísio Azevedo', 'https://m.media-amazon.com/images/I/71t2Ew0W14L.jpg', 'Um cortiço no Rio de Janeiro e suas histórias.', 'João Romão era um homem muito trabalhador... O cortiço crescia cada vez mais...');
+('O Cortiço', 'Aluísio Azevedo', 'https://covers.openlibrary.org/b/id/12749851-L.jpg', 'Um cortiço no Rio de Janeiro e suas histórias.', 'João Romão era um homem muito trabalhador... O cortiço crescia cada vez mais...');
